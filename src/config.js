ca4d2d5c72751fdb2058a12c028bd28eadba4a60
Sumit-Dhakal <gorkha202@gmail.com>
2024-01-19 20:51:40 +0545

    changes

--- src/config.js
+++ src/config.js
@@ -1,12 +1,7 @@
-// Configuration for TMDB
-// To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
+
 
 const API_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
-
-// Images
-// An image URL looks like this example:
-// http://image.tmdb.org/t/p/w780/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg
 
 const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
 
